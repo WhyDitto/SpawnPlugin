7fb69099365dbbca9479b40008b307f3c65fa78b
WhyDitto <theditto30@gmail.com>
2022-07-05 17:31:09 -0400

    Push

---
+++ src/main/java/ditto/spawn/Commands/deleteCommand.java
@@ -0,0 +1,21 @@
+package ditto.spawn.commands;
+
+import ditto.spawn.Utils.ChatUtil;
+import ditto.spawn.Utils.DataUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class deleteCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+           Player player =  (Player) sender;
+            DataUtil.getConfig().set("Spawn", null);
+            DataUtil.saveConfig();
+            player.sendMessage(ChatUtil.colorize("&aYou have successfully deleted your spawn!"));
+        }
+        return true;
+    }
+}
